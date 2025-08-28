10653b560f4cc94e3d692b30ac5b5f804fae4c38
qubadoff <admin@burncode.org>
2025-08-29 00:15:55 +0400

    BLog fix

--- app/Models/CarModel.php
+++ app/Models/CarModel.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class CarModel extends Model
 {
+    use HasTranslations;
+
     protected $table = 'car_models';
 
     protected $guarded = ['id'];
+
+    public array $translatable = ['name'];
 }
