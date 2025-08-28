0afee30d9515a6e8c310c5871e2325a1511290d8
qubadoff <admin@burncode.org>
2025-08-29 00:19:14 +0400

    BLog fix

--- app/Filament/Resources/CarModelResource/Pages/CreateCarModel.php
+++ app/Filament/Resources/CarModelResource/Pages/CreateCarModel.php
@@ -4,9 +4,19 @@ namespace App\Filament\Resources\CarModelResource\Pages;
 
 use App\Filament\Resources\CarModelResource;
 use Filament\Actions;
+use Filament\Actions\LocaleSwitcher;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCarModel extends CreateRecord
 {
+    use CreateRecord\Concerns\Translatable;
+
     protected static string $resource = CarModelResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make()
+        ];
+    }
 }
