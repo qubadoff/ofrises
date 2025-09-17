88a8a6c6185a6eccb697cb9b7ccf43782b8f71bf
qubadoff <admin@burncode.org>
2025-09-17 20:22:13 +0400

    ne

--- routes/api.php
+++ routes/api.php
@@ -62,7 +62,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::prefix('company')->group(function () {
-        Route::post('/getCompany', [CompanyController::class, 'getCompany']);
+        Route::get('/getCompany', [CompanyController::class, 'getCompany']);
         Route::post('/create', [CompanyController::class, 'create']);
     });
 });
