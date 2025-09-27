a36798dc26f634a23566db207156e3af90927a2f
qubadoff <admin@burncode.org>
2025-09-27 21:13:33 +0400

    ne

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Worker;
+use App\Observers\WorkerObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Worker::observe(WorkerObserver::class);
     }
 }
